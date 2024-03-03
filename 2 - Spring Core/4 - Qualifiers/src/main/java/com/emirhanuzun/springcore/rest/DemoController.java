696a45a9bc7ad7758cfa922dbee58a068eb97a90
Emirhan <1emirhanuzun@gmail.com>
2024-03-03 16:07:25 -0500

    Qualifiers Annotation was added

---
+++ 2 - Spring Core/4 - Qualifiers/src/main/java/com/emirhanuzun/springcore/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.emirhanuzun.springcore.rest;
+
+import com.emirhanuzun.springcore.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //Define a private field for the dependency
+    private Coach myCoach;
+
+    // You can use any method name
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
