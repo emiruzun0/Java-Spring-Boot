d70eb3dda7d3df1fefc69617d3c217fde7eeb5ea
Emirhan <1emirhanuzun@gmail.com>
2024-04-04 12:00:12 -0400

    String trimmer was added for whitespace errors

--- 6 - Spring MVC/10 - Validation Demo/src/main/java/com/emirhanuzun/mvc/controller/CustomerController.java
+++ 6 - Spring MVC/10 - Validation Demo/src/main/java/com/emirhanuzun/mvc/controller/CustomerController.java
@@ -3,15 +3,28 @@ package com.emirhanuzun.mvc.controller;
 import com.emirhanuzun.mvc.Customer;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+        
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
