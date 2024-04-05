a0a03b763bdeb9a1ab29aea296f2c29d877c7e1a
Emirhan <1emirhanuzun@gmail.com>
2024-04-04 22:41:40 -0400

    Custom login page and logout button were added

--- 8 - Spring Boot MVC Security/1 - Spring Boot MVC Security Default/src/main/java/com/emirhanuzun/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 8 - Spring Boot MVC Security/1 - Spring Boot MVC Security Default/src/main/java/com/emirhanuzun/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -50,6 +50,8 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
                 );
 
         return http.build();
