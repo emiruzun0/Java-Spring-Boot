8301a3fd1fe2e9de7ab16d7a50d75843070d9476
Emirhan <1emirhanuzun@gmail.com>
2024-04-06 15:15:10 -0400

    Find by id was added

--- 9 - JPA - Hibernate Advanced Mappings/1 - Jpa One To One Uni/src/main/java/com/emirhanuzun/cruddemo/CruddemoApplication.java
+++ 9 - JPA - Hibernate Advanced Mappings/1 - Jpa One To One Uni/src/main/java/com/emirhanuzun/cruddemo/CruddemoApplication.java
@@ -20,8 +20,20 @@ public class CruddemoApplication {
 		return runner -> {
 			//createInstructor(appDAO);
 
-			
+			findInstructor(appDAO);
 		};
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+
+		int theId = 1;
+		System.out.println("Finding instructor id : " + theId);
+
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+
+		System.out.println("Instructor : " + tempInstructor );
+		System.out.println("The associate instructor detail only : " + tempInstructor.getInstructorDetail());
+
 	}
 
 	private void createInstructor(AppDAO appDAO) {
