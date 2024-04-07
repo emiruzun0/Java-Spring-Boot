9883bd1dffb2b9925e005116731cd5533afc40ea
Emirhan <1emirhanuzun@gmail.com>
2024-04-06 22:53:47 -0400

    Detele course by id was added

--- 9 - JPA - Hibernate Advanced Mappings/3 - Jpa One To Many/src/main/java/com/emirhanuzun/cruddemo/dao/AppDAO.java
+++ 9 - JPA - Hibernate Advanced Mappings/3 - Jpa One To Many/src/main/java/com/emirhanuzun/cruddemo/dao/AppDAO.java
@@ -18,4 +18,5 @@ public interface AppDAO {
     void update(Instructor tempInstructor);
     void update(Course tempCourse);
     Course findCourseById(int theId);
+    void deleteCourseById(int theId);
 }
