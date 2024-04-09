52e3ca5ce2d060b60d02dd61d57b5828aa8a2d74
Emirhan <1emirhanuzun@gmail.com>
2024-04-08 22:03:27 -0400

    Match only with a specific class
    Edited annotation to match with just specific class.

--- 10 - Spring Boot AOP/1 - AOP Demo/src/main/java/com/emirhanuzun/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10 - Spring Boot AOP/1 - AOP Demo/src/main/java/com/emirhanuzun/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -10,7 +10,7 @@ public class MyDemoLoggingAspect {
 
     // This is where I add all of my related advices for logging
 
-    @Before("execution(public void addAccount())")
+    @Before("execution(public void com.emirhanuzun.aopdemo.dao.AccountDAO.addAccount())")
     public void beforeAddAccountAdvice(){
         System.out.println("\n====>>>> Executing @Before advice on addAccount()");
     }
