0981635a12b8b00df262bf69902e3c7753e2cb20
Emirhan <1emirhanuzun@gmail.com>
2024-04-09 15:19:26 -0400

    Added one more method with using same pointcut

--- 10 - Spring Boot AOP/2 - AOP Demo Pointcut Declaration/src/main/java/com/emirhanuzun/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10 - Spring Boot AOP/2 - AOP Demo Pointcut Declaration/src/main/java/com/emirhanuzun/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.emirhanuzun.aopdemo.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -17,6 +18,11 @@ public class MyDemoLoggingAspect {
     @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
         System.out.println("\n====>>>> Executing @Before advice on addAccount()");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics(){
+        System.out.println("\n====>>>> Performing API Analytics");
     }
 
 }
